cb3d33ce9c428f6aa535dc998ddf10bac711eca2
Karthikeya Somayajula <94534138+RetardRento@users.noreply.github.com>
2023-03-31 19:15:25 +0530

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -3,12 +3,11 @@ import os
 import discord
 from discord.ext import commands
 
-API_KEY = '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'
+API_KEY = 'YOUR_API_KEY'
 os.environ['OPENAI_KEY'] = API_KEY
 openai.api_key = os.environ['OPENAI_KEY']
 
-bot = commands.Bot(command_prefix='!')  # Prefix for the bot commands
-
+bot = commands.Bot(command_prefix='!')  
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
@@ -18,6 +17,4 @@ async def ask(ctx, *, question):
     response = openai.Completion.create(engine='text-davinci-003', prompt=question, max_tokens=200)
     await ctx.send(response['choices'][0]['text'])
 
-bot.run('MTA4NTE5NjA2NjY2MTU5NzIzNQ.GVBfRg.fuU-d13hw-PhxGI7BIYhMDB9YOjzqwFpbjzUKM')
-
-#MTA4NTE5NjA2NjY2MTU5NzIzNQ.GVBfRg.fuU-d13hw-PhxGI7BIYhMDB9YOjzqwFpbjzUKM
+bot.run('YOUR_DISCORD_AUTHENTICATION_KEY')
